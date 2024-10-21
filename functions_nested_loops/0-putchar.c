5097b23323cbf8112fb49c82fea1678138e146c1
THO-Gaetan <9771@holbertonstudents.com>
2024-10-21 22:16:52 +0200

    0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a quote about programming using puts function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+const char *str = "_putchar";
+while (*str)
+{
+_putchar(*str)
+}
+return (0); }
