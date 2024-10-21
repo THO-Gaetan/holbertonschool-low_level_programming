5bd928d050db3cc508fd1fd5c119da427cacbed4
THO-Gaetan <9771@holbertonstudents.com>
2024-10-21 22:35:04 +0200

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -2,10 +2,14 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints a quote about programming using puts function
- *
+ * Description: This program prints the string "_putchar" to the standard
+ * output using a custom _putchar function. It demonstrates the use of
+ * a while loop to iterate through a string and print each character.
+ * _putchar - writes the character c to stdout
+ * c: The str character that _putcher is writting
  * Return: Always 0 (Success)
  */
+int _putchar(char c);
 int main(void)
 {
 const char *str = "_putchar";
