ef80d4418f36165f88cc0585b92a7c365684b8b9
THO-Gaetan <9771@holbertonstudents.com>
2024-10-21 17:14:33 +0200

    2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints sizes of various types with specific formatting
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  while(i = 96; i <= 122)
+    {
+      putchar(i);
+      i++;
+    }
+  return (0);
+}
