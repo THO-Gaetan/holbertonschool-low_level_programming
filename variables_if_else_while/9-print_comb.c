942a3ed189b2cd2f812c7a6294dc5af5290bb7b6
THO-Gaetan <9771@holbertonstudents.com>
2024-10-21 19:50:31 +0200

    9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,14 +9,15 @@
 int main(void)
 {
 int i = 48;
+int v = 44;
 while (i <= 57)
 {
 putchar(i);
 i++;
 if (i != 57)
 {
-putchar("44");
-putchar(" ");
+putchar("v");
+putchar(' ');
 }
 }
 putchar('\n');
