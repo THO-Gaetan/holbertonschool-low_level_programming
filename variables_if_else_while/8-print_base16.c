3cfb3dc35924cd12a02ce9695bf62bca3821747a
THO-Gaetan <9771@holbertonstudents.com>
2024-10-21 19:37:27 +0200

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a quote about programming using puts function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 48;
+while (i <= 57)
+{
+putchar(i);
+i++;
+}
+i = 97;
+while (i <= 102)
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0); }
