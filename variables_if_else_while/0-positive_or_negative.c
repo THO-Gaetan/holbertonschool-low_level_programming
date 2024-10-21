bfb323f4c7434048b18df4adb6968d3fec834de1
THO-Gaetan <9771@holbertonstudents.com>
2024-10-21 15:22:47 +0200

    if

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a quote about programming using puts function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+if (n >= 0)
+printf("is positive");
+else if (n = 0)
+printf("is zero");
+else
+printf("is negative");
+return (0); }
