1e9065f1a3f48224fadc4d0dc3ded949bbf3de63
THO-Gaetan <9771@holbertonstudents.com>
2024-10-21 13:22:58 +0200

    5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a quote about programming using puts function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,")
+return (0); }
