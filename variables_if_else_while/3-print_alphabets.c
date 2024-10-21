58220ac97450963c5239afebd76e8e78ffc9628c
THO-Gaetan <9771@holbertonstudents.com>
2024-10-21 19:00:54 +0200

    Second alphabets

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a quote about programming using puts function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int i = 97;
+  while (i <= 122)
+    {
+      putchar(i);
+      i++;
+    }
+  i = 65;
+  while (i <= 90)
+    {
+      putchar(i);
+      i++;
+    }
+  putchar('\n');
+  return (0); }
