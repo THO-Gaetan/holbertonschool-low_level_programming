4912615c9abd85e88fc4f5b2b787ad7380fd66b7
THO-Gaetan <9771@holbertonstudents.com>
2024-10-21 19:02:38 +0200

    Second alphabets

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -8,17 +8,17 @@
  */
 int main(void)
 {
-  int i = 97;
-  while (i <= 122)
-    {
-      putchar(i);
-      i++;
-    }
-  i = 65;
-  while (i <= 90)
-    {
-      putchar(i);
-      i++;
-    }
-  putchar('\n');
-  return (0); }
+int i = 97;
+while (i <= 122)
+{
+putchar(i);
+i++;
+}
+i = 65;
+while (i <= 90)
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0); }
