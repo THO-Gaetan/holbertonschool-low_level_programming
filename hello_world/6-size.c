c63f0cc3aaed20fd2a54d5401e13e72254b1220d
THO-Gaetan <9771@holbertonstudents.com>
2024-10-21 14:22:17 +0200

    sizeof

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,5 +12,5 @@ printf("Size of a char: %u byte(s)\n", sizeof(char));
 printf("Size of a int: %u byte(s)\n", sizeof(int));
 printf("Size of a long int: %u byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %u byte(s)\n", sizeof(long int));
-printf("Size of a float: %u byte(s)\n", sizeof(float));
+printf("Size of a float: %u byte(s)", sizeof(float));
 return (0); }
