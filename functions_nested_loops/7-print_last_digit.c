d1e8443d8b6e54a6ab0aaa7fae5579cbdbdf9b0b
THO-Gaetan <9771@holbertonstudents.com>
2024-10-22 16:13:45 +0200

    7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * print_last_digit - Prints and returns the last digit of a number
+ * @i: The integer to extract the last digit from
+ *
+ * Description: This function extracts the last digit of the input integer,
+ * prints it using _putchar, and returns its value. It works for both
+ * positive and negative integers.
+ *
+ * Return: The value of the last digit
+ */
 int print_last_digit(int i)
 {
 int n;
