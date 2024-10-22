3ecd892edecce34837b967e4e74c1cf531006225
THO-Gaetan <9771@holbertonstudents.com>
2024-10-22 09:19:46 +0200

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+* _putchar - writes a character to stdout
+* @c: the character to be written
+*
+* Description:
+* This function writes a single character to the standard output stream
+* It takes a character as an argument and returns an integer value represente
+* the character written. If an error occurs, it returns EOF (End Of File).
+*
+* Return: Always 0 (Success)
+*/
+int _putchar(char c)
+int main(void)
+{
+const char *str="abcdefghijklmnopqrstuvwxyz\n"
+  while (*str)
+    {
+      _putchar(*str);
+      *str++;
+    }
+ return (0); }
