bb68407f710420870b992b5e408d2594fecd4d9f
THO-Gaetan <9771@holbertonstudents.com>
2024-10-22 11:55:32 +0200

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -13,10 +13,10 @@
 */
 void print_alphabet(void)
 {
-  char str;
-  for (str = 'a'; str <= 'z'; str++)
-    {
-      _putchar(str);
-    }
-  _putchar('\n');
-  return; }
+char str;
+for (str = 'a'; str <= 'z'; str++)
+{
+_putchar(str);
+}
+_putchar('\n');
+return; }
