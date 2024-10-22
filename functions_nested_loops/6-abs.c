e35f956bc2911671d3d9bfaf5ec2561d6a07f1b1
THO-Gaetan <9771@holbertonstudents.com>
2024-10-22 15:48:03 +0200

    6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,12 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * _abs - Computes the absolute value of an integer
+ * @i: The integer to compute the absolute value of
+ *
+ * Description: This function calculates the absolute value of the input
+ * integer. If the input is non-negative, it returns the input as is.
+ * If the input is negative, it returns the negation of the input.
+ *
+ * Return: The absolute value of the input integer
+ */
 int _abs(int i)
 {
-  if (i >= 0)
-    {
-      return (i);
-    }
-  return (-i);
+if (i >= 0)
+{
+return (i);
+}
+return (-i);
 }
