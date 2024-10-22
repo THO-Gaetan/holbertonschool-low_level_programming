f72d5711174b628ddaf208cdb9c626b5fee61e15
THO-Gaetan <9771@holbertonstudents.com>
2024-10-22 12:14:25 +0200

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -3,9 +3,14 @@
 
 void print_alphabet_x10(void)
 {
+  int n = 1;
+  while (n <= 10)
+    {
   const char *str = "abcdefghijklmnopqrstuvwxyz\n";
   while (*str)
     {
       _putchar(*str++);
 	}
+  n++
+  }
   return; }
