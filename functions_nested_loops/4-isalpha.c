7f34082691a7e2544dabbfa1acda0676ab534991
THO-Gaetan <9771@holbertonstudents.com>
2024-10-22 14:08:06 +0200

    4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _islower - Checks for lowercase character
- * @c: The character to check
+ * _isalpha - Checks for alphabetic character
+ * @c: The character to be checked
  *
  * Description: This function determines whether a given character is
- * lowercase or not. It checks if the ASCII value of the character falls
- * within the range of lowercase letters ('a' to 'z').
+ * an alphabetic character (lowercase or uppercase). It checks if the
+ * ASCII value of the character falls within the ranges of lowercase
+ * letters ('a' to 'z') or uppercase letters ('A' to 'Z').
  *
- * Return: 1 if c is lowercase, 0 otherwise
+ * Return: 1 if c is a letter (lowercase or uppercase), 0 otherwise
  */
 int _isalpha(int c)
 {
