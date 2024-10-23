797a4a9ff63506e85c74c2e1c19dded562451a32
THO-Gaetan <9771@holbertonstudents.com>
2024-10-23 11:31:11 +0200

    8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -10,11 +10,11 @@ void jack_bauer(void)
     {
       while (minute < 60)
 	{
-	  _putchar(hour / 10) + '0';
-	  _putchar(hour % 10) + '0';
+	  _putchar(hour / 10)+ '0';
+	  _putchar(hour % 10)+ '0';
 	  _putchar(':');
-	  _puthcar(minute / 10) + '0';
-	  _putchar(minute % 10) + '0';
+	  _puthcar(minute / 10)+ '0';
+	  _putchar(minute % 10)+ '0';
 	  minute++;
 	}
       hour ++
