14580e0657265f3f6792e0b3536ec4da8923c6bd
THO-Gaetan <9771@holbertonstudents.com>
2024-10-23 17:03:31 +0200

    11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -5,6 +5,21 @@ void print_to_98(int n)
 {
   while (n != 98)
     {
+    if (n < 0 && n > -10)
+    {
+      _putchar(n - '0');
+    }
+  else if (n <= -10 && n > -100)
+    {
+      _putchar((n / 10) - '0');
+      _putchar((n % 10) - '0');
+    }
+  else if (n <= -100 && n > -1000)
+    {
+      _putchar((n / 100) - '0');
+      _putchar(((n / 10) % 10) - '0');
+      _putchar((n % 10) - '0');
+    }
   if (n < 10)
     {
       _putchar(n + '0');
