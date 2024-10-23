da33a1da571814d75a9ee184f2788ec37942b014
THO-Gaetan <9771@holbertonstudents.com>
2024-10-23 16:10:59 +0200

    10

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -3,7 +3,15 @@
 
 int add(int a,int b)
 {
-  a = add(a, b);
-  _putchar((a / 10) + '0');
+  a = a + b;
+  if (a < 10)
+    {
+    _putchar(a + '0');
+    }
+  else
+    {
+      _putchar((a / 10) + '0');
+      _putchar((a % 10) + '0');
+    }
   return (0);
 }
