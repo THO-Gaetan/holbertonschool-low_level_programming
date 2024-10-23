87011929fb987a534241467410e84d182b290541
THO-Gaetan <9771@holbertonstudents.com>
2024-10-23 13:04:00 +0200

    9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+void times_table(void)
+{
+  int table, result;
+  table = 0;
+  result = 0;
+  while (table <= 9)
+    {
+      while (result <= 9)
+	{
+	  _putchar(result, '0');
+	  if (result != 9)
+	    {
+	  _putchar(", ");
+	    }
+	  result++;
+	}
+      table++;
+    }
+  return;
+}
