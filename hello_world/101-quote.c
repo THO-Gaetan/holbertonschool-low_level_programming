f1aac9273917db627317f39b9713c91e3335b42c
THO-Gaetan <9771@holbertonstudents.com>
2024-10-24 11:08:14 +0200

    9

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a quote by Dora Korpar to standard output
+ * Return: Always 1 (Error)
+ */
 int main(void)
 {
 char *dora = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
