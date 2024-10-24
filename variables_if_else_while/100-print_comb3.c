b9d7b9316a7db8ef6ee9904636cd5540fd5445cf
THO-Gaetan <9771@holbertonstudents.com>
2024-10-24 11:19:38 +0200

    10

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -5,8 +5,8 @@ void comb3(void)
   int two_digit;
   for (two_digit = 01, two_digit <= 89, two_digit++)
     {
-      putchar(i);
-      if ( i !=89)
+      putchar(two_digit);
+      if ( two_digit !=89)
 	{
 	  putchar(44);
           putchar(32);
