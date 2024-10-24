19e7dd726d72f030fe2af5c6c31b26656d5ccee6
THO-Gaetan <9771@holbertonstudents.com>
2024-10-24 14:40:44 +0200

    10

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -1,52 +1,24 @@
 #include <stdio.h>
 
-/*int main(void)
-{
-  int two_digit;
-  int i;
-  for (two_digit = 01; two_digit <= 89; two_digit++)
-    {
-      if (two_digit < 10)
-	{
-	  i = 0;
-	  while (i <=1)
-	    {
-	      i = two_digit * i;
-	      putchar(i + '0');
-	      i++;
-	    }
-	}
-      else
-	{
-	  putchar((two_digit / 10) + '0');
-	  putchar((two_digit % 10) + '0');
-	}
-      if ( two_digit !=89)
-	{
-	  putchar(',');
-	  putchar(' ');
-	}
-    }
-  return (0);
-  }*/
 int main(void)
 {
-    int tens, ones;
-
-    for (tens = 0; tens <= 8; tens++)
-    {
-        for (ones = tens + 1; ones <= 9; ones++)
-        {
-            putchar(tens + '0');
-            putchar(ones + '0');
-            
-            if (!(tens == 8 && ones == 9))
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
-    return (0);
+  int second, one;
+  second = 0;
+  while (second <= 8)
+  {
+    one = 1;
+    while (one <= 9)
+      {
+	putchar(second + '0');
+	putchar(one + '0');
+	one++;
+      }
+    if (second != 8 && one != 9)
+      {
+	putchar(',');
+	putchar(' ');
+      }
+    second++;
+  }
+  return (0);
 }
