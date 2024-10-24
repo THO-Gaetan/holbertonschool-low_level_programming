fe8338c9215a237ce4bb24142ea394c265da7962
THO-Gaetan <9771@holbertonstudents.com>
2024-10-24 11:07:27 +0200

    9

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -3,8 +3,7 @@
 
 int main(void)
 {
-char *reduce = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
- const char *dora = reduce;
+char *dora = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(1, dora, 59);
 return (1);
 }
