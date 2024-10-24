0990b3dd42eb5223c970228e13ec8c9404961b4e
THO-Gaetan <9771@holbertonstudents.com>
2024-10-24 13:12:30 +0200

    10

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -5,7 +5,10 @@ int main(void)
   int two_digit;
   for (two_digit = 01; two_digit <= 89; two_digit++)
     {
-      putchar(two_digit + '0');
+      if {two_digit < 10)
+	{
+	  
+      putchar(two_digit + '00');
       if ( two_digit !=89)
 	{
 	  putchar(44);
