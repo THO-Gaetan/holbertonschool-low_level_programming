99e449281b715ff53ed2440f6109c3d85da36634
THO-Gaetan <9771@holbertonstudents.com>
2024-10-26 10:46:54 +0200

    10

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int quatre, trois, deux, un;
+  for (quatre = 0; quatre <= 9; quatre++)
+    {
+      for (trois = 0; trois <= 8; trois++)
+	{
+	  for (deux = 0; deux <= 9; deux++)
+	    {
+	      for (un = trois + 1; un <=9; un++)
+		{
+		  if (!(un == 1 && deux == 0 && trois == 0 && quatre == 0))
+		    {
+		      putchar(',');
+		      putchar(' ');
+		    }
+		  putchar(quatre + '0');
+		  putchar(trois + '0');
+		  putchar(' ');
+		  putchar(deux + '0');
+		  putchar(un + '0');
+		}
+	    }
+	}
+    }
+  putchar('\n');
+  return (0);
+}
