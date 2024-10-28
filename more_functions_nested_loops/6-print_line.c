55eecd6131ab1e34a1932fd270f6805c3acf0e65
THO-Gaetan <9771@holbertonstudents.com>
2024-10-28 10:56:07 +0100

    6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void print_line(int n)
+{
+  while (n > 0)
+    {
+      _putchar(95);
+      n--;
+    }
+  while (n <= 0)
+    {
+      _putchar('\n');
+    }
+  _putchar('\n');
+  return;
+}
