3d5c7c66a94a6f03ef635a2c922a97f21009812e
THO-Gaetan <9771@holbertonstudents.com>
2024-10-28 09:33:21 +0100

    12

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+void print_times_table(int n)
+{
+  int col = 0;
+  int raw = 0;
+  while ( col <= n)*
+    {
+      if (n > 15 && n < 0)
+	{
+	  break;
+	}
+      while (raw <= n)
+	{
+	  putchar(n + '0');
+	  if (raw != n)
+	    {
+	      putchar(',');
+	      putchar(' ');
+	    }
+	  raw++;
+	}
+      col++;
+    }
+  return;
+}
