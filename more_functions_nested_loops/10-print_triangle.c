38d18b63f6eae98f2bbeccc420ef0856d0ed0205
THO-Gaetan <9771@holbertonstudents.com>
2024-10-28 13:38:24 +0100

    10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,33 +1,38 @@
 #include "main.h"
-
+/**
+ * print_triangle - Prints a triangle, followed by a new line
+ * @s: Size of the triangle
+ *
+ * Description: If size is 0 or less, the function should print only a new line
+ */
 void print_triangle(int s)
 {
-  int col = s;
-  int raw;
-  int len = 1;
-  while (col > 0)
-    {
-      int sp = col - len;
-      raw = s;
-      while (raw > 0)
-	{
-	  if (sp != 0)
-	    {
-	      _putchar(' ');
-	      sp--;
-	    }
-	  else
-	    {
-	      _putchar(35);
-	    }
-	  raw--;
-	}
-      _putchar('\n');
-      col--;
-    }
-  if (s <= 0)
-    {
-      _putchar('\n');
-    }
-  return;
+int col = s;
+int raw;
+int len = 1;
+while (col > 0)
+{
+int sp = col - len;
+raw = s;
+while (raw > 0)
+{
+if (sp != 0)
+{
+_putchar(' ');
+sp--;
+}
+else
+{
+_putchar(35);
+}
+raw--;
+}
+_putchar('\n');
+col--;
+}
+if (s <= 0)
+{
+_putchar('\n');
+}
+return;
 }
