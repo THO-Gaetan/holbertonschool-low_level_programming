39a3762a09927892a662b156ac72a46f0cb5790a
THO-Gaetan <9771@holbertonstudents.com>
2024-10-28 12:31:27 +0100

    8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -3,12 +3,13 @@
 void print_square(int s)
 {
   int col = s;
-  int raw = s;
+  int raw;
   if (s > 0)
     {
-      while (col >= 0)
+      while (col > 0)
 	{
-	  while (raw >= 0)
+	  raw = s;
+	  while (raw > 0)
 	    {
 	      _putchar(35);
 	      raw--;
