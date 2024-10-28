9e04f39b585b70496fe56e8a19659eb51677bc9b
THO-Gaetan <9771@holbertonstudents.com>
2024-10-28 13:11:53 +0100

    9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,49 +1,43 @@
 #include <stdio.h>
-
+/**
+ * main - FizzBuzz program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-  int len = 1;
-  while (len <= 100)
-    {
-      if(len >= 10 && len <= 100)
-	{
-	  if ((len % 5) == 0 && (len % 3) == 0)
-	    {
-	      printf("FizzBuzz");
-	    }
-	  else if ((len % 5) == 0)
-	    {
-	      printf("Buzz");
-	    }
-	  else if ((len % 3) == 0)
-	    {
-	      printf("Fizz");
-	    }
-	  else
-	    {
-	      putchar((len / 10) + '0');
-	      putchar((len % 10) + '0');
-	    }
-	}
-      else if (len >= 1 && len < 10)
-	{
-	  if ((len % 5) == 0)
-	    {
-	      printf("Buzz");
-	    }
-	  else if ((len % 3) == 0)
-	    {
-	      printf("Fizz");
-	    }
-	  else
-	    {
-	  putchar(len + '0');
-	    }
-	}
-      if (len != 100)
-	{
-	  putchar(' '); }
-      len++;
-    }
-  putchar('\n');
-  return (0); }
+int len = 1;
+while (len <= 100)
+{
+if (len >= 10 && len <= 100)
+{
+if ((len % 5) == 0 && (len % 3) == 0)
+{
+printf("FizzBuzz"); }
+else if ((len % 5) == 0)
+{
+printf("Buzz"); }
+else if ((len % 3) == 0)
+{
+printf("Fizz"); }
+else
+{
+putchar((len / 10) + '0');
+putchar((len % 10) + '0'); } }
+else if (len >= 1 && len < 10)
+{
+if ((len % 5) == 0)
+{
+printf("Buzz"); }
+else if ((len % 3) == 0)
+{
+printf("Fizz"); }
+else
+{
+putchar(len + '0'); } }
+if (len != 100)
+{
+putchar(' '); }
+len++; }
+putchar('\n');
+return (0); }
