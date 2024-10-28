f0c91f59c38398b19fd3ea09d46bc30c2d20868f
THO-Gaetan <9771@holbertonstudents.com>
2024-10-28 12:59:49 +0100

    9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,20 +1,42 @@
-#include "main.h"
+#include <stdio.h>
 
-void more_numbers(void)
+int main(void)
 {
   int len = 0;
   while (len <= 100)
     {
       if(len >= 10 && len < 100)
 	{
-	  _putchar((len / 10) + '0');
-	  _putchar((len % 10) + '0');
+	  if((len % 5) == 0)
+	    {
+	      printf("Fizz");
+	    }
+	  else if ((len % 3) == 0)
+	    {
+	      printf("Buzz");
+	    }
+	  else
+	    {
+	      putchar((len / 10) + '0');
+	      putchar((len % 10) + '0');
+	    }
 	}
       else if (len >= 1 && len < 10)
 	{
-	  _putchar(len + '0');
+	  if ((len % 5) == 0)
+	    {
+	      printf("Buzz");
+	    }
+	  else if ((len % 3) == 0)
+	    {
+	      printf("Fizz");
+	    }
+	  else
+	    {
+	  putchar(len + '0');
+	    }
 	}
-      _putchar(' ');
+      putchar(' ');
       len++;
     }
 }
