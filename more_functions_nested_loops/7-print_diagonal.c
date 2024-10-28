8439b7ce4295444ef25f13b32d5354167a744653
THO-Gaetan <9771@holbertonstudents.com>
2024-10-28 12:14:14 +0100

    7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -6,12 +6,15 @@ void print_diagonal(int n)
   while (n >= 0)
     {
     _putchar(92);
-    _putchar('\n');
-    len = n + 1;
-    while (len >= 0)
+    if (n != 0)
       {
-	_putchar(' ');
-	len--;
+	_putchar('\n');
+	len = n + 1;
+	while (len >= 0)
+	  {
+	    _putchar(' ');
+	    len--;
+	  }
       }
     n--;
     }
