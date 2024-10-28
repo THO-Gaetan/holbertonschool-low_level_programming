f92a597a92fbc2f624e0106a5aa42cc8755bf163
THO-Gaetan <9771@holbertonstudents.com>
2024-10-28 12:35:01 +0100

    8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,27 +1,32 @@
 #include "main.h"
-
+/**
+ * print_square - Prints a square, followed by a new line
+ * @size: Size of the square
+ *
+ * Description: If size is 0 or less, the function should print only a new line
+ */
 void print_square(int s)
 {
-  int col = s;
-  int raw;
-  if (s > 0)
-    {
-      while (col > 0)
-	{
-	  raw = s;
-	  while (raw > 0)
-	    {
-	      _putchar(35);
-	      raw--;
-	    }
-	  col--;
-	  _putchar('\n');
-	}
-    }
-  if (s <= 0 && raw == s)
-    {
-      _putchar('\n');
-    }
-  return;
+int col = s;
+int raw;
+if (s > 0)
+{
+while (col > 0)
+{
+raw = s;
+while (raw > 0)
+{
+_putchar(35);
+raw--;
+}
+col--;
+_putchar('\n');
+}
+}
+if (s <= 0 && col == s)
+{
+_putchar('\n');
+}
+return;
 }
   
