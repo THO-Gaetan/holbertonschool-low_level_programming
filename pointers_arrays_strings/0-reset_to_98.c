e6979544708d8f9a8b5edfb377f5075574a1495b
THO-Gaetan <9771@holbertonstudents.com>
2024-10-29 09:57:21 +0100

    0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,20 @@
 #include "main.h"
-
+/**
+ * reset_to_98 - Resets the value pointed to by n to 98
+ * @n: Pointer to an integer to be reset
+ *
+ * Description: This function takes a pointer to an integer and
+ * modifies the value it points to, setting it to 98.
+ */
 void reset_to_98(int *n)
 {
-  while (*n > 98)
-    {
-      (*n)--;
-    }
-  while (*n < 98)
-    {
-      (*n)++;
-    }
-  return;
+while (*n > 98)
+{
+(*n)--;
+}
+while (*n < 98)
+{
+(*n)++;
+}
+return;
 }
