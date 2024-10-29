18cb795a77717c7350caa7b03c79e0b3209ce192
THO-Gaetan <9771@holbertonstudents.com>
2024-10-29 12:43:56 +0100

    5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,15 +1,17 @@
 #include "main.h"
+#include <string.h>
 
 void rev_string(char *s)
 {
   int len = strlen(s);
   char temp;
   int i = 0;
-  while (i < len - 1; i++)
+  while (i < len - 1)
     {
-      temp = str[i];
-      str[i] = str[len - i - 1];
-      str[leng - i - 1] = temp;
+      temp = s[i];
+      s[i] = s[len - i - 1];
+      s[len - i - 1] = temp;
+      i++;
     }
   return;
 }
