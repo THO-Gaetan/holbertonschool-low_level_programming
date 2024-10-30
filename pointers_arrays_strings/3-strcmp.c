05c057353fa59b8401fbfe979ddb18e7a0226286
THO-Gaetan <9771@holbertonstudents.com>
2024-10-30 21:10:30 +0100

    3v2

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -2,7 +2,7 @@
 
 int _strcmp(char *s1, char *s2)
 {
-  while {*s1 && *s2)
+  while (*s1 && *s2)
     {
       if (*s1 != *s2)
 	{
@@ -11,5 +11,5 @@ int _strcmp(char *s1, char *s2)
       s1++;
       s2++;
     }
-    return (*s1 - *s2);
+  return (*s1 - *s2);
 }
