a9f02e3a76a9c0b2171bd0b2ed409d34d88e4e5f
THO-Gaetan <9771@holbertonstudents.com>
2024-10-30 14:49:27 +0100

    0v2

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+char *_strcat(char *dest, char *src)
+{
+  int n = 0;
+  int run = 0;
+  int runv2 = 0;
+  int *oups = dest;
+  int *help = src;
+  int sum;
+  while (*oups != '\n')
+    {
+      oups++;
+      runv2++;
+    }
+  runv2--;
+  while (*help != '\0')
+    {
+      help++;
+      run++;
+    }
+  run--;
+  sum = run + runv2;
+  runv2 = run + 1;
+  while (run <= sum)
+    {
+      dest[runv2] = src[n];
+      n++;
+      runv2++;
+      run++;
+    }
+  return (dest);
+}
