7f26df2174dd6a4e60176319b7343e0cdbe1129b
THO-Gaetan <9771@holbertonstudents.com>
2024-10-30 21:09:59 +0100

    3v2

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -2,34 +2,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-  char *oups = s1;
-  char *help = s2;
-  int run = 0;
-  int runv2 = 0;
-  while (*oups)
+  while {*s1 && *s2)
     {
-      run++;
-      oups++;
+      if (*s1 != *s2)
+	{
+	  return (*s1 - *s2);
+	}
+      s1++;
+      s2++;
     }
-  while (*help)
-    {
-      runv2++;
-      help++;
-    }
-  if (run > runv2)
-    {
-      _putchar('-');
-      _putchar('1');
-      _putchar('3');
-    }
-  else if (run < runv2)
-    {
-      _putchar('1');
-      _putchar('3');
-    }
-  else if (run == runv2)
-    {
-      _putchar('0');
-    }
-  return (0);
+    return (*s1 - *s2);
 }
