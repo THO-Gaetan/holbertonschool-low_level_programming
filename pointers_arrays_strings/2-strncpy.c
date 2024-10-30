26335b03242364ad46cd0c87e99751aafd425383
THO-Gaetan <9771@holbertonstudents.com>
2024-10-30 20:00:33 +0100

    2v2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,18 +1,24 @@
 #include "main.h"
-
+/**
+ * _strncpy - Copies a string
+ * @dest: The destination string
+ * @src: The source string
+ * @n: The number of bytes to copy
+ *
+ * Return: A pointer to the resulting string dest
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
-  int run = 0;
-  
-  while (run < n && src[run] != '\0')
-    {
-      dest[run] = src[run];
-      run++;
-    }
-  while (run < n)
-    {
-      dest[run] = '\0';
-      run++;
-    }
-  return (dest);
+int run = 0; 
+while (run < n && src[run] != '\0')
+{
+dest[run] = src[run];
+run++;
+}
+while (run < n)
+{
+dest[run] = '\0';
+run++;
+}
+return (dest);
 }
