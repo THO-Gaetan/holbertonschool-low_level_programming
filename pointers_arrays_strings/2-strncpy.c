5b3b467da0ecc0f7a7f2277e19f06d26b4df3d20
THO-Gaetan <9771@holbertonstudents.com>
2024-10-30 19:07:47 +0100

    2v2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -4,16 +4,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
   char *help = src;
   int run = 0;
-  int runv2 = 0;
-  while (*help != '\0')
+  
+  while (run < n && src[run] != '\0')
     {
-      help++;
+      dest[run] = src[run];
       run++;
-    }
-  while (runv2 <= run && runv2 < n)
-    {
-      dest[runv2] = src[runv2];
-      runv2++;
     }
   return (dest);
 }
