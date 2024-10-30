00c09af6fe176d7a8371f9b5ab211cf409100acc
THO-Gaetan <9771@holbertonstudents.com>
2024-10-30 18:58:07 +0100

    2v2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+char *_strncpy(char *dest, char *src, int n)
+{
+  char *help = src;
+  int run = 0;
+  int runv2 = 0;
+  while (*help != '\0')
+    {
+      help++;
+      run++;
+    }
+  run--;
+  while (runv2 <= run && runv2 < n)
+    {
+      dest[runv2] = src[runv2];
+      runv2++;
+    }
+  return (dest);
+}
