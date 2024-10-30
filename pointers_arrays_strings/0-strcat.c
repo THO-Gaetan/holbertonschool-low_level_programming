9b7e5cf0ceff49e2d15160b306f923e5cfce38a1
THO-Gaetan <9771@holbertonstudents.com>
2024-10-30 15:56:27 +0100

    0v2

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,31 +1,37 @@
 #include "main.h"
-
+/**
+ * _strcat - Concatenates two strings
+ * @dest: The destination string
+ * @src: The source string to be appended
+ *
+ * Return: A pointer to the resulting string dest
+ */
 char *_strcat(char *dest, char *src)
 {
-  int n = 0;
-  int run = 0;
-  int runv2 = 0;
-  char *oups = dest;
-  char *help = src;
-  int sum;
-  while (*oups != '\0')
-    {
-      oups++;
-      runv2++;
-    }
-  runv2--;
-  while (*help != '\0')
-    {
-      help++;
-      run++;
-    }
-  sum = run + runv2;
-  runv2++;
-  while (runv2 <= sum)
-    {
-      dest[runv2] = src[n];
-      n++;
-      runv2++;
-    }
-  return (dest);
+int n = 0;
+int run = 0;
+int runv2 = 0;
+char *oups = dest;
+char *help = src;
+int sum;
+while (*oups != '\0')
+{
+oups++;
+runv2++;
+}
+runv2--;
+while (*help != '\0')
+{
+help++;
+run++;
+}
+sum = run + runv2;
+runv2++;
+while (runv2 <= sum)
+{
+dest[runv2] = src[n];
+n++;
+runv2++;
+}
+return (dest);
 }
