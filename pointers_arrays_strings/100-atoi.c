683a70afc27b4e44a5db99905e9233bcd9343022
THO-Gaetan <9771@holbertonstudents.com>
2024-10-30 09:37:50 +0100

    10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -4,35 +4,23 @@ int _atoi(char *s)
 {
   int i = 0;
   int neg = 1;
-  char n = '0';
+  char n;
   while (*s != '\0')
     {
+      n = '0';
       while (n <= '9')
 	{
-	  if (*s == ' ')
-	    {
-	      s++;
-	    }
-	  else if (*s == '-')
+	  if (*s == '-')
 	    {
 	      neg = -1;
-	      s++;
-	    }
-	  else if (*s == '+')
-	    {
-	      s++;
 	    }
 	  else if (*s == n)
 	    {
 	      i = i * 10 +(*s - '0');
-	      s++;
-	    }
-	  else
-	    {
-	      s++;
 	    }
 	  n++;
 	}
+      s++;
     }
   return (neg * i);
 }
