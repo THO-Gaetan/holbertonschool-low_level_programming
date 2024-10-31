095997587470c075982c0869284b534ea3df0f1f
THO-Gaetan <9771@holbertonstudents.com>
2024-10-31 16:34:36 +0100

    7v2

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,24 +1,33 @@
 #include "main.h"
-
+/**
+ * leet - Encodes a string into 1337
+ * @str: The string to be encoded
+ *
+ * Description: This function replaces specific letters with numbers:
+ * 'a' and 'A' with '4', 'e' and 'E' with '3', 'o' and 'O' with '0',
+ * 't' and 'T' with '7', 'l' and 'L' with '1'.
+ *
+ * Return: A pointer to the encoded string
+ */
 char *leet(char *str)
 {
-  char *s = str;
-  char *oups = "aAeEoOtTlL";
-  char *help = "4433007711";
-  int i = 0;
-  int n;
-  while (s[i] != '\0')
-    {
-      n = 0;
-      while (oups[n] != '\0')
-	{
-	  if (s[i] == oups[n])
-	    {
-	      str[i] = help[n];
-	    }
-	  n++;
-	}
-      i++;
-    }
-  return (str);
+char *s = str;
+char *oups = "aAeEoOtTlL";
+char *help = "4433007711";
+int i = 0;
+int n;
+while (s[i] != '\0')
+{
+n = 0;
+while (oups[n] != '\0')
+{
+if (s[i] == oups[n])
+{
+str[i] = help[n];
+}
+n++;
+}
+i++;
+}
+return (str);
 }
