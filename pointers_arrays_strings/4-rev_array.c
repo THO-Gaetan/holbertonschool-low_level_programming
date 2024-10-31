3dbb0568960f1340e54abe4e35f5a4e0857249c0
THO-Gaetan <9771@holbertonstudents.com>
2024-10-31 10:11:09 +0100

    4v2

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -5,13 +5,13 @@ void reverse_array(int *a, int n)
   int OUPS = 0;
   int HELP = n;
   int ALAID;
-  while (OUPS < HELP)
+  while (OUPS <= HELP)
     {
       ALAID = a[OUPS];
       a[OUPS] = a[HELP];
       a[HELP] = ALAID;
       OUPS++;
-      HELP++;
+      HELP--;
     }
   return;
 }
