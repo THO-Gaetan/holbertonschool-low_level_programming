7bf8a80d6d83be9845a4524bccf7bef2bfdad41d
THO-Gaetan <9771@holbertonstudents.com>
2024-11-04 10:30:41 +0100

    0v3

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-void *_memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 int i = 0
 while (i < n)
@@ -8,5 +8,4 @@ while (i < n)
 s[i] = b
 i++;
 }
-return;
 }
