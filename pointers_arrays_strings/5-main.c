9ee321fd88fad307e060b23d3bf97b8b4a6d74fe
THO-Gaetan <9771@holbertonstudents.com>
2024-11-05 09:36:55 +0100

    5v3

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-    char *s = "hello, world";
+    char *s = "hello, world Maybe the day is bright";
     char *f = "world";
     char *t;
 
--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -9,34 +9,34 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-int i = 0, n = 0, maybe = 0, stop = 0;
+    int i = 0, n = 0, maybe = 0, stop = 0;
 
-char *help = haystack;
+    char *help = haystack;
 
-while (help[i] != '\0')
-{
-if (needle[n] == ' ')
-{
-stop = 1;
-}
-if (stop == 0)
-{
-if (help[i] == needle[n])
-{
-n++;
-maybe++;
-}
-else if (n != 0)
-{
-n = 0;
-haystack = haystack + maybe + 1;
-}
-else
-{
-haystack++;
-}
-}
-i++;
-}
-return (haystack);
+    while (help[i] != '\0')
+        {
+            if (needle[n] == '\0')
+            {
+                stop = 1;
+            }
+            if (stop == 0)
+            {
+                if (help[i] == needle[n])
+                {
+                    n++;
+                    maybe++;
+                }
+                else if (n != 0)
+                {
+                    n = 0;
+                    haystack = haystack + maybe + 1;
+                }
+                else
+                {
+                    haystack++;
+                }
+            }
+            i++;
+        }
+    return (haystack);
 }
