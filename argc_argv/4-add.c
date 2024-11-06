960f737b74797ab605e0d6d89f3e5af2522bc323
THO-Gaetan <9771@holbertonstudents.com>
2024-11-06 14:40:20 +0100

    4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+/**
+ * main - Adds positive and negative numbers
+ * @argc: Number of command line arguments
+ * @argv: Array of command line arguments
+ *
+ * Description: This program adds all numbers provided as command line
+ * arguments. It handles both positive and negative numbers. If no numbers
+ * are provided, it prints 0. If any argument is not a valid number, it
+ * prints "Error" and returns 1.
+ *
+ * Return: 0 if successful, 1 if error
+ */
 int main(int argc, char *argv[])
 {
 	int i = 1, j;
@@ -20,7 +31,7 @@ int main(int argc, char *argv[])
 		if (relay == 0 && j != 0)
 		{
 			printf("Error\n");
-			return(1);
+			return (1);
 		}
 		if (relay < 0)
 			sum += (-1 * relay);
