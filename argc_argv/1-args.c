70212f25a48d116f9e057af2d457798d53bd4c83
THO-Gaetan <9771@holbertonstudents.com>
2024-11-06 10:39:30 +0100

    1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * @argc: The number of command line arguments
+ * @argv: An array containing the command line arguments
+ *
+ * Description: This program counts the number of arguments passed to it
+ * (excluding the program name) and prints the count.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc && *argv)
+	{
+	i++;
+	}
+	printf("%d", i - 1);
+	return (0);
+}
