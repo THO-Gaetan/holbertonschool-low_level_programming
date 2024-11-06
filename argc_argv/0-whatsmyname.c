7335c55f4e32358cffc33003a3487f310752c847
THO-Gaetan <9771@holbertonstudents.com>
2024-11-06 10:27:50 +0100

    0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
-
-int main (int argc, char *argv[])
-{	
+/**
+ * main - Entry point of the program
+ * @argc: The number of command line arguments
+ * @argv: An array containing the command line arguments
+ *
+ * Description: This program prints the name of the executable (argv[0])
+ * if there are any command line arguments.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
 	if (argc > 0)
 	printf("%s\n", argv[0]);
 	return (0);
