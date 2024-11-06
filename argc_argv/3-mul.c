72c9282b5e8b991dd49a81e78257d0409f240b34
THO-Gaetan <9771@holbertonstudents.com>
2024-11-06 11:09:27 +0100

    3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - Entry point of the program
+ * @argc: The number of command line arguments
+ * @argv: An array containing the command line arguments
+ *
+ * Description: This program multiplies two numbers provided as command line
+ * arguments. If the number of arguments is not exactly 3 (program name and
+ * two numbers), it prints "Error" and returns 1. Otherwise, it multiplies
+ * the two numbers and prints the result.
+ *
+ * Return: 0 if successful, 1 if error
+ */
 int main(int argc, char *argv[])
 {
 	int sum;
 
 	if (argc != 3)
-	printf("Error\n");
-	return (1);
+	{
+		printf("Error\n");
+		return (1);
+	}
 	else
 	{
 		sum = atoi(argv[1]) * atoi(argv[2]);
-		printf ("%d\n", sum);
+		printf("%d\n", sum);
 	}
 	return (0);
 }
