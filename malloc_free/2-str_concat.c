650c9e6411764e642a649b25be0fe8ac22f0232f
THO-Gaetan <9771@holbertonstudents.com>
2024-11-11 17:23:09 +0100

    2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * str_concat_help - Calculates the total length of two strings
+ * @s1: The first string
+ * @s2: The second string
+ *
+ * Return: The total length of s1 and s2
+ */
 int str_concat_help(char *s1, char *s2)
 {
 	int i = 0, n = 0;
@@ -12,12 +18,23 @@ int str_concat_help(char *s1, char *s2)
 	i += n;
 	return (i);
 }
-
+/**
+ * str_concat - Concatenates two strings
+ * @s1: The first string
+ * @s2: The second string
+ *
+ * Return: A pointer to the newly allocated space in memory containing
+ *         the concatenated string, or NULL if it fails
+ */
 char *str_concat(char *s1, char *s2)
 {
 	int i = 0, n = 0;
 	char *p = malloc(str_concat_help(s1, s2) + 1);
 
+	if (!p)
+	{
+		return (NULL);
+	}
 	while (s1[i] != '\0')
 	{
 		p[i] = s1[i];
@@ -29,5 +46,6 @@ char *str_concat(char *s1, char *s2)
 		n++;
 		i++;
 	}
+	p[i] = '\0';
 	return (p);
 }
