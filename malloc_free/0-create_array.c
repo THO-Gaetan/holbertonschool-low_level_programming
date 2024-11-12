b655e4a23b21dbb3b90e9c105f740be2bd4d4176
THO-Gaetan <9771@holbertonstudents.com>
2024-11-12 14:51:54 +0100

    0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
 
-	char *p = malloc(size * sizeof(char) + 1);
+	char *p = malloc(size * sizeof(char));
 
 	if (!p)
 	{
@@ -24,6 +24,5 @@ char *create_array(unsigned int size, char c)
 		p[i] = c;
 		i++;
 	}
-	p[i] = '\0';
 	return (p);
 }
