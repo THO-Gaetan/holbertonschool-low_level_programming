7076d404878147ebbab8a36a07b9e648d86297f6
THO-Gaetan <9771@holbertonstudents.com>
2024-11-12 15:32:15 +0100

    4

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,7 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
 
-void free_grid(int **arr, int height)
+void free_grid(int **grid, int height)
 {
-	
+	for (; height > 0; height--)
+	{
+		free(grid[height]);
+	}
+	free(grid);
 }
