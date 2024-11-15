54ef329f7f9a60a90f7137a467b4f3143c8dd2d9
THO-Gaetan <9771@holbertonstudents.com>
2024-11-15 17:53:11 +0100

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,11 +1,19 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
+char *dog_change_data(char *data_changed, char *data, int increment, int len)
+{
+	while (increment < len)
+	{
+		data_changed[increment] = data[increment];
+		increment++;
+	}
+	return (data_changed);
+}
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	int len1, len2;
+	int len1, len2, nbname = 0, nbowner = 0;
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
@@ -24,8 +32,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog);
 		return (NULL);
 	}
-	strcpy(dog->name, name);
-	strcpy(dog->owner, owner);
+	dog_change_data(dog->name, name, nbname, len1);
+	dog_change_data(dog->owner, owner, nbowner, len2);
 	dog->age = age;
 	return (dog);
 }
