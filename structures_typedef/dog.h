2c1b1c1fa387205515b1acfc26a1e8a81e48b8b5
THO-Gaetan <9771@holbertonstudents.com>
2024-11-15 13:58:59 +0100

    3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,20 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog - A dog's basic info
- * @name: First member
- * @age: Second member
- * @owner: Third member
+ * struct dog - A structure representing a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
  *
- * Description: Longer description
+ * Description: This structure holds information about a dog,
+ * including its name, age, and owner.
  */
-struct dog_t dog
+typedef struct dog_t
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-void print_dog(struct dog *d);
+} dog;
 
 #endif
