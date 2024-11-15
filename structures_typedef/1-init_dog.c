dcd1c9e68e02444a4c05d63cc32f500cff93115f
THO-Gaetan <9771@holbertonstudents.com>
2024-11-15 12:30:36 +0100

    1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,10 @@
+#include "dog.h"
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog my_dog;
+
+	my_dog.name = "Poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
+}
