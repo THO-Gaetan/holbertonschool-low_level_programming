b6f88949805865bbf7475ac5c4cc974265677c78
THO-Gaetan <9771@holbertonstudents.com>
2024-11-16 15:44:29 +0100

    14

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
-
+/**
+ * main - Prints the first 50 Fibonacci numbers
+ *
+ * Description: This program calculates and prints the first 50 numbers
+ * in the Fibonacci sequence. The numbers are printed as integers,
+ * separated by commas and spaces.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	float i = 0, f1 = 1, f2 = 2, temp;
 
 	while (i < 50)
 	{
-		printf("%.0f = %.0f\n", i + 1, f1);
+		printf("%.0f, ", f1);
 		temp = f2;
 		f2 += f1;
 		f1 = temp;
