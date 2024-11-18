425e8aeac2223a053c623546402c3f67cd09c446
THO-Gaetan <9771@holbertonstudents.com>
2024-11-18 13:04:58 +0100

    0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,6 +1,14 @@
 #include "function_pointers.h"
 #include <stdlib.h>
-
+/**
+* print_name - print the name of the function.
+* @name : the name of my function.
+* @f : the function pointers that will be used.
+* Description : the function will check if name and f is null
+* if name is null it will just return
+* else it will give name as the parameter for the function pointer f
+* return: void
+*/
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL)
