0710d6b57c696879f39c9629e5efdcd40e0927e8
THO-Gaetan <9771@holbertonstudents.com>
2024-11-18 14:12:04 +0100

    2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -17,8 +17,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-	if (array == 0 || cmp == 0)
-		return (0);
+	if (array == NULL || cmp == NULL)
+		return (-1);
 	while (i < size)
 	{
 		if (cmp(array[i]) != 0)
