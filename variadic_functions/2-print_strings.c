5daa6707971fd0012f032b34433c2d240136e921
THO-Gaetan <9771@holbertonstudents.com>
2024-11-19 13:19:22 +0100

    3

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -4,23 +4,24 @@
  * print_strings - start of the variadic function
  * @separator : the variable that print the separator
  * @n : the variable that stock the number of parameter that follow
- * Description: print all the number stocked after the int n, separate by the
- * separator variable.
+ * Description: print all the parameter stocked after the int n, separate by
+ * the separator variable.
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i = 0;
-	char *res;
 
 	va_start(ap, n);
 
 	while (i < n)
 	{
-		res = va_arg(ap, char *);
 		if (separator && i != 0)
 			printf("%s", separator);
-		printf("%s", res);
+		if (separator[i] != 0)
+			printf("%s", va_arg(ap, char *));
+		else
+			printf("(nil)");
 		i++;
 	}
 	printf("\n");
