a27996c445b1e4dc7e932b3f6d2777f72543f514
THO-Gaetan <9771@holbertonstudents.com>
2024-11-19 12:35:14 +0100

    0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,6 +1,12 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
-
+/**
+ * sum_them_all - start of the variadic function
+ * @n : variable that contain the number of parameter who follow.
+ * Description: Prints a quote about programming using puts function
+ *
+ * Return: always return sum at the end
+ */
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
