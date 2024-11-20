6381889a45d239e383c957a85c9a5c2ae31e611a
THO-Gaetan <9771@holbertonstudents.com>
2024-11-20 11:13:46 +0100

    changing how I print my ', ' to add a checking if format is null

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -11,39 +11,40 @@
 void print_all(const char * const format, ...)
 {
 	int i = 0;
-	char *str;
+	char *str, *sep = "";
 	va_list ap;
 
+	if (format == NULL)
+		return (0);
 	va_start(ap, format);
 	while (format[i] != '\0')
 	{
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(ap, int));
+				printf("%s%c", sep, va_arg(ap, int));
 				break;
 			case 'i':
-				printf("%d", va_arg(ap, int));
+				printf("%s%d", sep, va_arg(ap, int));
 				break;
 			case 'f':
-				printf("%f", va_arg(ap, double));
+				printf("%s%f", sep, va_arg(ap, double));
 				break;
 			case 's':
 				str = va_arg(ap, char*);
 				if (str == NULL)
 				{
-					printf("(nil)");
+					printf("%s(nil)", sep);
 					break;
 				}
-				printf("%s", str);
+				printf("%s%s", sep, str);
 				break;
 			default:
 				i++;
 				continue;
 		}
+		sep = ", ";
 		i++;
-		if (format[i])
-			printf(", ");
 	}
 	va_end(ap);
 	printf("\n");
