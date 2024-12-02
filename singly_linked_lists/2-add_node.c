613dd25092672992be566f46bd32f14948f71c48
THO-Gaetan <9771@holbertonstudents.com>
2024-12-02 12:03:46 +0100

    correcting the output

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,5 +2,26 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	
+	list_t *head;
+	list_t *new;
+	list_t hello = {str, str_len(str), NULL};
+	size_t n;
+
+	head = &hello;
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		printf("Error\n");
+		return (-1);
+	}
+	new->
+}
+
+size_t str_len(const char *str)
+{
+	int len = 0;
+
+	while (str[len])
+		len++;
+	return (len);
 }
