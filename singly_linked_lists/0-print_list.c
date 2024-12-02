e21f1fce30f6d8b1b2713bbdc7105e11bf2292bd
THO-Gaetan <9771@holbertonstudents.com>
2024-12-02 10:41:29 +0100

    adding the commentary

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,5 +1,13 @@
 #include "lists.h"
-
+/**
+ * print_list - Write and counts the number of elements in a linked list
+ * @h: Pointer to the head of the list
+ *
+ * Description: This function traverses a linked list and write every nodes and
+ * counts the number of nodes in it.
+ *
+ * Return: The number of elements in the list
+ */
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,5 +1,13 @@
 #include "lists.h"
-
+/**
+ * list_len - Counts the number of elements in a linked list
+ * @h: Pointer to the head of the list
+ *
+ * Description: This function traverses a linked list and counts
+ * the number of nodes in it.
+ *
+ * Return: The number of elements in the list
+ */
 size_t list_len(const list_t *h)
 {
 	unsigned int count = 0;
