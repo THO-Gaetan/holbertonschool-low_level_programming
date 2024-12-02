0275648a1067b8f970be814181c1879e262638a6
THO-Gaetan <9771@holbertonstudents.com>
2024-12-02 15:12:04 +0100

    adding the commentary

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,5 +1,11 @@
 #include "lists.h"
-
+/**
+ * free_list - Frees a list_t list
+ * @head: Pointer to the head of the list
+ *
+ * Description: This function frees all nodes of a linked list,
+ * including the strings within each node.
+ */
 void free_list(list_t *head)
 {
 	list_t *temp;
