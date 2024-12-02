3a6ed6e3eaf239c583ccc49903cf08d97f4a9dd2
THO-Gaetan <9771@holbertonstudents.com>
2024-12-02 20:24:45 +0100

    Test adding a checker

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,6 +13,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	temp = *h;
+	if (temp == NULL)
+		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
