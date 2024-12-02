ce67e6952cc8157ef283848df239df008e004ca6
THO-Gaetan <9771@holbertonstudents.com>
2024-12-02 19:44:22 +0100

    trying to add more precise condition after my while loop

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -30,9 +30,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	if (temp == NULL)
 		return (-1);
-	if (temp != NULL)
+	if (temp->prev != NULL)
 		temp->prev->next = temp->next;
-	if (temp != NULL)
+	if (temp->next != NULL)
 		temp->next->prev = temp->prev;
 	free(temp);
 	return (1);
