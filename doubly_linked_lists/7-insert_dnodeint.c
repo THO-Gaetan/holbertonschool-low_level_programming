aed962ac55232fd8c4798e3b7866993b4c90e53a
THO-Gaetan <9771@holbertonstudents.com>
2024-12-02 18:32:28 +0100

    adding commentary and check for condition if temp is null

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
-
+/**
+ * insert_dnodeint_at_index - Inserts a new node at a given position
+ * @h: Pointer to a pointer to the head of the doubly linked list
+ * @idx: Index where the new node should be added
+ * @n: Integer value to be stored in the new node
+ *
+ * Return: Address of the new node, or NULL if it failed or index is out range
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new, *temp;
@@ -23,6 +30,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		temp = temp->next;
 		i++;
+	}
+	if (temp == NULL)
+	{
+		free(new);
+		return (NULL);
 	}
 	new->next = temp->next;
 	if (temp->next != NULL)
