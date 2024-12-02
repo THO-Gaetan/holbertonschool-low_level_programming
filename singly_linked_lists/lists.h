c0bf4b9c1fcc6805106934476cb64d21e3520bc7
THO-Gaetan <9771@holbertonstudents.com>
2024-12-02 10:06:52 +0100

    structuring the header

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,6 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+typedef struct lists
+{
+char *str;
+unsigned int len;
+struct lists *node;
+} lists_t;
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
