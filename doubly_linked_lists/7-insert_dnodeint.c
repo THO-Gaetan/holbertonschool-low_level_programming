cb7186aad9550d262fe8b9a383ef3a0171d0acd5
THO-Gaetan <9771@holbertonstudents.com>
2024-12-02 20:28:23 +0100

    changing *h for temp in the loop

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new;
 		return (new);
 	}
-	while (*h != NULL && i < idx - 1)
+	while (temp != NULL && i < idx - 1)
 	{
 		temp = temp->next;
 		i++;
