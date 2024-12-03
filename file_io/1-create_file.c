6f0307b7d02d3e071efbc624514be30001506466
THO-Gaetan <9771@holbertonstudents.com>
2024-12-03 15:55:15 +0100

    checking if text is null

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,11 +17,14 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
-	bytes_written = write(fd, text_content, str_len(text_content));
-	if (bytes_written == -1)
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (-1);
+		bytes_written = write(fd, text_content, str_len(text_content));
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 	close(fd);
 	return (1);
