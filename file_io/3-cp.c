13519c2da8ecd15f675ab750b6e494a53bb365ee
THO-Gaetan <9771@holbertonstudents.com>
2024-12-06 21:56:41 +0100

    checking to add a checker for error

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -33,6 +33,8 @@ int main(int argc, char *argv[])
 
 	while ((bytes_read = read(fd_from, box, BOX_SIZE)) > 0)
 	{
+		if (bytes_read == -1)
+			error_exit(98, "Error: Can't read from file %s\n", argv[1]);
 		bytes_written = write(fd_to, box, bytes_read);
 		if (bytes_written == -1 || bytes_written != bytes_read)
 			error_exit(99, "Error: Can't write to %s\n", argv[2]);
